6666a69ca4364ca066f28e690224e9c5325c5386
XGHeaven <619943612@qq.com>
2015-09-29 01:30:52 +0800

    add some test

--- test/withEnv/index.js
+++ test/withEnv/index.js
@@ -1,12 +1,16 @@
 var expect = require('chai').expect;
+var env_deploy = require('../..');
 
-describe('with env', function() {
-    before('clear env', function() {
-        delete process.env.NODE_ENV;
-        delete process.env.MONGODB;
-    });
+function clearEnv() {
+    //process.env = {}
+    // no use
+}
+
+describe('with env use json format', function() {
+    var env = process.env;
+    before('clear env', clearEnv);
     it('should be load .env success', function() {
-        var result = require('../../')(__dirname);
+        var result = env_deploy(__dirname);
         expect(result).to.be.ok;
     });
     it('base check', function() {
@@ -16,5 +20,45 @@ describe('with env', function() {
     it('nested obejct to check', function() {
         expect(process.env.A_B).to.be.eql('b');
         expect(process.env.A_C_D).to.be.eql('1');
+    });
+});
+
+describe('with env use key=value format', function() {
+    var env = process.env;
+    before('clear env', clearEnv);
+
+    it('should be load .env success', function() {
+        expect(env_deploy(__dirname + '/.env-key-value')).to.be.ok;
+    });
+
+    it('right value', function() {
+        expect(env).to.be.contains({
+            FIRST: '1',
+            SECOND: 'two',
+            WITHEQL: 'la la = 123'
+        });
+    });
+
+    it('invalid value', function() {
+        expect(env).to.be.not.contains({
+            ERR: null
+        });
+    });
+});
+
+describe('set autoTransformCase to false', function() {
+    var env = process.env;
+    before('load', function() {
+        env_deploy(__dirname + '/.env-key-value', {
+            autoTransformCase: false
+        });
+    });
+
+    it('test', function() {
+        expect(env).to.be.contains({
+            first: '1',
+            second: 'two',
+            withEql: 'la la = 123'
+        });
     });
 });
